9747ee5692615d6107a35b94eca5e985e6e7749d
Abdelrahman-Mamdouh16 <bedo.lela32@gmail.com>
2025-01-08 11:21:01 +0200

    expo eject

---
+++ metro.config.js
@@ -0,0 +1,10 @@
+/**
+ * Metro configuration for Expo
+ * https://docs.expo.dev/guides/customizing-metro/
+ *
+ * @format
+ */
+
+const { getDefaultConfig } = require('expo/metro-config');
+
+module.exports = getDefaultConfig(__dirname);
